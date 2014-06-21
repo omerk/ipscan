49ade756afbbbdb19563a12a87604e87566a8289
Omer Kilic <omerkilic@gmail.com>
2014-06-21 15:20:53 +0100

    add Linux support.

--- ipscan.py
+++ ipscan.py
@@ -1,4 +1,4 @@
-import os, platform, subprocess, threading, time, urllib
+import sys, os, platform, subprocess, threading, time, urllib
 from tabulate import tabulate
 from datetime import datetime, timedelta
 
@@ -6,7 +6,7 @@ ping_results = []
 netbios_results = []
 oui_db = {}
 
-class nbtstat_thread(threading.Thread):
+class netbios_thread(threading.Thread):
 	def __init__(self, ip):
 		threading.Thread.__init__(self)
 		self.ip = ip
@@ -15,13 +15,16 @@ class nbtstat_thread(threading.Thread):
 		_NULL = open(os.devnull, 'wb')
 		if platform.system() == "Windows":
 			nbtstat_proc = subprocess.Popen(['nbtstat', '-A', str(self.ip)], stdout=subprocess.PIPE)
-			out, err = nbtstat_proc.communicate()
+		elif platform.system() == "Linux":
+			nbtstat_proc = subprocess.Popen(['nmblookup', '-A', str(self.ip)], stdout=subprocess.PIPE)
+
+		out, err = nbtstat_proc.communicate()
 			
-			for line in out.split("\n"):
-				if "UNIQUE" in line:
-					if "<00>" in line: # Looking for "Workstation Service"
-						r = line.strip().split()
-						netbios_results.append([self.ip, r[0].replace("<00>","")])
+		for line in out.split("\n"):
+			if not "GROUP" in line:
+				if "<00>" in line: # Looking for "Workstation Service"
+					r = line.strip().split()
+					netbios_results.append([self.ip, r[0].replace("<00>","")])
 			
 class ping_thread(threading.Thread):
 	def __init__(self, ip):
@@ -32,6 +35,9 @@ class ping_thread(threading.Thread):
 		_NULL = open(os.devnull, 'wb')
 		if platform.system() == "Windows":
 			subprocess.Popen(["ping", "-n", "2", str(self.ip)], stdout=_NULL, stderr=_NULL)
+		elif platform.system() == "Linux":
+			subprocess.Popen(["ping", "-c", "2", str(self.ip)], stdout=_NULL, stderr=_NULL)
+
 
 def download_oui_txt():
 	print "Downloading oui.txt..."
@@ -43,9 +49,9 @@ def oui_lookup(mac):
 
 if __name__ == '__main__':
 	print "ipscan.py 0.9 <https://github.com/omerk/ipscan>\n"
-	
+
 	ping_threads = []
-	nbtstat_threads = []
+	netbios_threads = []
 	
 	# Ensure that oui.txt exists and it is not older than 2 weeks
 	if os.path.exists("oui.txt"):
@@ -75,34 +81,43 @@ if __name__ == '__main__':
 	# Wait for the ARP cache to settle(??)
 	print "Parsing ARP cache..."
 	time.sleep(5)
-	
+
 	# Go through the ARP cache
 	if platform.system() == "Windows":
 		arp_proc = subprocess.Popen(['arp', '-a'], stdout=subprocess.PIPE)
 		out, err = arp_proc.communicate()
-		
+	
 		# Pick hosts and dump then in the results array
 		for line in out.split("\n"):
 			if 'dynamic' in line:
 				r = line.strip().split()
 				ping_results.append([r[0], "-", r[1], oui_lookup(r[1][:8].replace("-",""))])
+	elif platform.system() == "Linux":
+		arp_proc = subprocess.Popen(['cat', '/proc/net/arp'], stdout=subprocess.PIPE)
+		out, err = arp_proc.communicate()
+
+		# Pick hosts and dump then in the results array
+		for line in out.split("\n"):
+			if '0x2' in line:
+				r = line.strip().split()
+				ping_results.append([r[0], "-", r[3], oui_lookup(r[3][:8].replace(":",""))])
+
+	# Create the list of threads for the Netbios scan
+	for r in ping_results:
+		netbios_threads.append(netbios_thread(r[0]))
 	
-		# Create the list of threads for the Netbios scan
-		for r in ping_results:
-			nbtstat_threads.append(nbtstat_thread(r[0]))
+	# Query Netbios names
+	print "Resolving Netbios Names..."
+	[t.start() for t in netbios_threads]
+	# Wait for completion
+	[t.join() for t in netbios_threads]
 		
-		# Query Netbios names
-		print "Resolving Netbios Names..."
-		[t.start() for t in nbtstat_threads]
-		# Wait for completion
-		[t.join() for t in nbtstat_threads]
-		
-		# Match ping and netbios scan results
-		# there has to be a better way of doing this...
-		for p in ping_results:
-			for n in netbios_results:
-				if p[0] == n[0]:
-					p[1] = n[1]
+	# Match ping and netbios scan results
+	# there has to be a better way of doing this...
+	for p in ping_results:
+		for n in netbios_results:
+			if p[0] == n[0]:
+				p[1] = n[1]
 
 	# Print results
 	print "\nTotal hosts found: %d\n" % len(ping_results)
